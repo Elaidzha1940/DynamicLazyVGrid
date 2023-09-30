dabbbbca0fb87a0d474e18cd054b5f5924726d84
Elaidzha1940 <eli080293@gmail.com>
2023-10-01 00:08:16 +0300

    DynamicLazyVGrid

---
+++ DynamicLazyVGrid/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DynamicLazyVGrid
+//
+//  Created by Elaidzha Shchukin on 01.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DynamicLazyVGrid/DynamicLazyVGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicLazyVGridApp.swift
+//  DynamicLazyVGrid
+//
+//  Created by Elaidzha Shchukin on 01.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct DynamicLazyVGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

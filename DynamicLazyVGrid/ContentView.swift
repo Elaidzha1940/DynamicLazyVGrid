97e4dbc8bd04176e24b950b8bb76dd52b3a7895c
Elaidzha1940 <eli080293@gmail.com>
2023-10-02 01:30:17 +0300

    Updated heightSecond

--- DynamicLazyVGrid/ContentView.swift
+++ DynamicLazyVGrid/ContentView.swift
@@ -10,15 +10,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var Height: CGFloat = 120
+    @State var heightSecond: CGFloat = 150
+    
+    let colors : [Color] = [.black.opacity(0.1), .black.opacity(0.2), .black.opacity(0.3), .black.opacity(0.4), .black.opacity(0.5), .black.opacity(0.6), .black.opacity(0.7), .black.opacity(0.8), .black.opacity(0.9), .black.opacity(0.1)]
+    
     var body: some View {
         
-        VStack {
-            LazyVGrid(columns: [GridItem(.adaptive(minimum: 200))]) {
-                ForEach(0..<10) {tem in
+        ScrollView {
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: Height), spacing: 10)], spacing: 10) {
+                ForEach(0..<10) {item in
                     RoundedRectangle(cornerRadius: 20, style: .continuous)
-                        .frame(height: 100)
+                        .frame(height: heightSecond)
+                        .foregroundColor(colors[item % colors.count])
                 }
             }
+            .padding(.horizontal)
         }
     }
 }
